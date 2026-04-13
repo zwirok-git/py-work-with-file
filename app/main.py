274957b3566393ac49bcaec2e7ce35adca52017f
zwirok-git <oleksijprokopenko@gmail.com>
2026-04-13 19:31:29 +0300

    implemented "create_report" function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    file_dict = {"supply": 0, "buy": 0, "result": 0}
+
+    with open(data_file_name, "r") as file:
+        for line in file:
+            if not line.strip():
+                continue
+
+            operation, amount = line.strip().split(",")
+            file_dict[operation] = file_dict[operation] + int(amount)
+
+    file_dict["result"] = file_dict["supply"] - file_dict["buy"]
+
+    with open(report_file_name, "w") as new_file:
+        for key, value in file_dict.items():
+            new_file.write(f"{key},{value}\n")
